8004319b4b7947622256dec4f7bdbc682b39c7db
Sumanth Yedoti <sumanth.yedoti@gmail.com>
2022-06-25 22:36:26 +0530

    neetcode(js): Solve contains-duplicate

---
+++ neetcode/arrays-hashing/contains-duplicate.js
@@ -0,0 +1,10 @@
+function containsDuplicates(arr) {
+  const items = {};
+  for (let i of arr) {
+    if (i in items) return true;
+    else items[i] = null;
+  }
+  return false;
+}
+
+module.exports = containsDuplicates;
