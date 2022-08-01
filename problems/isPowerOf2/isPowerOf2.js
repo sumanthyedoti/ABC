db34a5598f17d6ab8f2d5b7f822ecd0dbd7d3312
Sumanth Yedoti <sumanth.yedoti@gmail.com>
2022-08-01 22:08:19 +0530

    problems: isPowerOf2

---
+++ problems/isPowerOf2/isPowerOf2.js
@@ -0,0 +1,10 @@
+function isPowerOf2(n) {
+  if (n < 1) return false;
+  for (let x = n; x > 1; x /= 2) {
+    if (x % 2 !== 0) return false;
+  }
+  return true;
+}
+
+console.log(isPowerOf2(64));
+console.log(isPowerOf2(66));
