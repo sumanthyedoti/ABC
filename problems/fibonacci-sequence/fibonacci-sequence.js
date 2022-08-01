45b69a57d5aa658a0ce2cefab4c86a655628a71d
Sumanth Yedoti <sumanth.yedoti@gmail.com>
2022-08-01 21:44:28 +0530

    Factorial

--- problems/fibonacci-sequence/fibonacci-sequence.js
+++ problems/fibonacci-sequence/fibonacci-sequence.js
@@ -1,11 +1,12 @@
 function fibonacciSequence(n) {
   // keep track of previous two values
-  // keep adding until n
   let n1 = 0,
     n2 = 1;
   const seq = [n1, n2];
+  // keep adding until n
   for (let i = 2; i < n; i++) {
     const current = n1 + n2;
+    // update previous two values
     n1 = n2;
     n2 = current;
     seq.push(current);
