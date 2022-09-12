e4658ff71a7ec9c487df647aa6e0589ba4102367
Sumanth Yedoti <sumanth.yedoti@gmail.com>
2022-09-12 10:47:51 +0530

    problems: sum of primes

---
+++ problems/sum-of-primes/sum-of-primes.js
@@ -0,0 +1,23 @@
+function isPrime(n) {
+  if(n < 2) return false
+  for(let i = 2; i <= Math.sqrt(n); i++) {
+    if(n % i === 0) {
+      return false
+    }
+  }
+  return true
+}
+
+function sumOfPrimes(limit) {
+  let sum = 0
+  for(let n = 2; n <= limit; n++) {
+    if(isPrime(n)) {
+      sum += n
+    }
+  }
+  return sum
+}
+
+console.log(sumOfPrimes(15)); // 41
+console.log(sumOfPrimes(10)); // 17
+console.log(sumOfPrimes(20)); // 77
